5b20ce99ec3154f9ac89fc6e076444f336fde8ca
Jmlundeen <jmlundee@yahoo.com>
2022-03-13 20:33:19 -0500

    Add splash screen

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.view import View
 from src.controller import Controller
 from src.model import Model
+from src.splashscreen import SplashScreen
 # Main function
 if __name__=='__main__':
-
+	splash = SplashScreen()
 	model = Model()
 	model.get_db_cred()
 	view = View()
