8306b1ebf2bf9ca64643e0e9b6be54c19a2461fa
jacksondirienzo <JJTDiRienzo0@gmail.com>
2022-04-04 10:35:04 -0500

    Cleaned code; splash is now resizable

--- src/splashscreen.py
+++ src/splashscreen.py
@@ -9,79 +9,108 @@ class SplashScreen:
 	#default constructor
 	def __init__(self):
 
-        #Opens logo image using pill
+        #Opens using pill
 		self.logo = Image.open('src/gui_assets/Logo.png')
 
-		#Opens background image using pill
+		#Opens using pill
 		self.background = Image.open("src/gui_assets/background.png")
 
-		self.image_resizing()
-		self.displaySplash()
+		root.title("Splash Screen")
 
+		self.run()
 
-	#Method to resize the logo and background image
-	def image_resizing(self):
+	def run(self):
 
-		#Get the current screen width and height
+		self.grab_screen_dimensions()
+
+		self.set_window_dimensions()
+
+		self.resize_background()
+
+		self.resize_logo()
+
+		self.set_canvas_placement()
+
+		self.create_canvas()
+
+		self.display_splash()
+
+	def grab_screen_dimensions(self):
+
 		self.screen_width = root.winfo_screenwidth()
+
 		self.screen_height = root.winfo_screenheight()
 
-		#Resizes the background image to screen dimensions
-		bg_resize = self.background.resize((self.screen_width, self.screen_height))
+	def set_window_dimensions(self):
 
-		#Sets resized image under Tkinter
-		self.background = ImageTk.PhotoImage(bg_resize)
+		self.window_width = 900
+
+		self.window_height = 600
+
+		#Holds screen center dimensions
+		self.center_window_width = (self.screen_width/2) - (self.window_width/2)
+		self.center_window_height = (self.screen_height/2) - (self.window_height/2)
+
+	def resize_background(self):
+
+		#Resizes the background image to screen dimensions
+		resized_background = self.background.resize((self.screen_width, self.screen_height))
+
+		#Set with Tkinter
+		self.background = ImageTk.PhotoImage(resized_background)
+
+	def resize_logo(self):
 
 		#Grabs logo's width and height
 		logo_width, logo_height = self.logo.size
 
-		#Ratio used to either reduce or enlarge the logo
+		#Ratio used to multiply
 		resize_logo_ratio = 1.4
 
-		#Changes the logo's dimensions by the resize_ratio
-		logo_width = int(logo_width * resize_logo_ratio)
-		logo_height = int(logo_height * resize_logo_ratio)
+		resized_logo_width = int(logo_width * resize_logo_ratio)
+		resized_logo_height = int(logo_height * resize_logo_ratio)
 
-		#Resizes the logo to the new dimensions with pillow
-		resize_logo = self.logo.resize((logo_width, logo_height))
+		#Resizes logo with pillow
+		resized_logo = self.logo.resize((resized_logo_width, resized_logo_height))
 
-		#Sets resized logo  under Tkinter
-		self.logo = ImageTk.PhotoImage(resize_logo)
+		self.logo = ImageTk.PhotoImage(resized_logo)
 
 
+	def set_canvas_placement(self):
 
-	#displays the splash screen with logo for 3 seconds and then closes
-	def displaySplash(self):
+		self.canvas_text_width = self.window_width/2
 
-		#Sets window title of screen
-		root.title("Splash Screen")
+		self.canvas_image_width = self.window_width/2
 
-		#Text displayed on splash screen
+		self.canvas_image_height = (self.window_height/2 - (self.window_height * 0.27))
+
+		self.canvas_text_height = self.window_height/2
+
+	def create_canvas(self):
+
 		splash_text = "Studio 12"
 
-		#Determines the length in seconds of how long the splash screen will be on screen
-		splash_duration = 5
-
-		#Creates a canvas for text and images, highlightthickness gets rid of white border around it
+		#create canvas
 		canvas = Canvas(root, highlightthickness = 0)
 
-		#Creates the canvas background image; anchor just sets top let point (0,0) to northwest of screen
 		canvas.create_image(0, 0, image = self.background, anchor = 'nw')
 
-		#Creaets canvas text; first 2 parameters are location; fill is text color; anchor centers horizontally (like in a word document)
-		canvas.create_text(self.screen_width/2, (self.screen_height/2 - (self.screen_height * 0.27)), text = splash_text, font= 'silom 80 bold', fill = 'white', anchor = 'center')
+		canvas.create_text(self.canvas_text_width, self.canvas_image_height, text = splash_text, font= 'silom 80 bold', fill = 'white', anchor = 'center')
 
-		#Creates canvas logo image; should be lower than canvas text
-		canvas.create_image(self.screen_width/2, self.screen_height/2, image = self.logo, anchor = 'center')
+		canvas.create_image(self.canvas_image_width, self.canvas_text_height, image = self.logo, anchor = 'center')
 
-		#Packs canvas to screen; fill & pack make the canvas and objects fill to their size rather than get cut off
+		#Fill & pack has the canvas and objects fill to their size rather than get cut short
 		canvas.pack(fill = "both", expand = True)
 
-		#Sets splash screen to fullscreen
-		root.wm_attributes('-fullscreen', 'True')
+	def display_splash(self):
+
+		#In seconds
+		splash_screen_duration = 2
+
+		root.geometry('%dx%d+%d+%d' % (self.window_width, self.window_height, self.center_window_width, self.center_window_height))
 
 		#after specfied time destroy splash screen
-		root.after(splash_duration * 1000, root.destroy)
+		root.after(splash_screen_duration * 1000, root.destroy)
 
 		#Holds splash open until duration runs out
 		root.mainloop()
